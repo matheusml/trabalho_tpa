ff431094d2d50d93dc44c3e9d20cebfb81eb39a5
matheusml <matheusml90@gmail.com>
2013-03-02 12:32:37 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrabalhoTpa::Application.config.secret_token = 'ab7faa9cc9b02088f06e1897ea17f896042bebf34d4e9db5bcce6fc60d0b5c28fdb004c262df6de45e6e02ce665600dbd63b40a265df28712259a66c82b1b28e'
