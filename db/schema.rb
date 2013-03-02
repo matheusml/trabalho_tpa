5ca75108e17e84bbae184689a5a9523497246e96
matheusml <matheusml90@gmail.com>
2013-03-02 14:43:03 -0300

    Adds customer_id to buy

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302173828) do
+ActiveRecord::Schema.define(:version => 20130302174204) do
 
   create_table "buys", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "customer_id"
   end
 
   create_table "cart_items", :force => true do |t|
