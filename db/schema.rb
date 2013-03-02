0ff77072d7b39b24c237b102fa0b2c6342bbcb90
matheusml <matheusml90@gmail.com>
2013-03-02 14:27:07 -0300

    Adds product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302154137) do
+ActiveRecord::Schema.define(:version => 20130302172226) do
+
+  create_table "products", :force => true do |t|
+    t.float    "price"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
