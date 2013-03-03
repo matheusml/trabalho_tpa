ed99dee5ba9f5b9d8079a86849cdc6f5744b3b65
matheusml <matheusml90@gmail.com>
2013-03-03 00:21:21 -0300

    Adds carts helpers

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,10 @@
 module CartsHelper
+	def sum_of_products(cart_items)
+		sum = 0
+		cart_items.each do |cart_item|
+			product = Product.find(cart_item.product_id)
+			sum += product.price
+		end
+		number_to_currency(sum, :unit => "R$")
+	end
 end
