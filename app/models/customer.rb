0ae2c1d32f7aae3bec65a217729a137e80d5e436
matheusml <matheusml90@gmail.com>
2013-03-17 12:03:16 -0300

    Creates customers points logic

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,11 @@ class Customer < ActiveRecord::Base
 	validates_presence_of :cpf
 		
 	has_many :buys
+
+	def update_points
+		if self.preferential
+			self.points ? self.points += 10 : self.points = 10
+			self.save
+		end
+	end
 end
